d41d9639cf93ecbab7519cbc757be6c91655219c
Engghgtytr <enzogamer.penchel@gmail.com>
2026-05-20 11:40:35 -0300

    Atualizar o inss.js

--- inss.js
+++ inss.js
@@ -1,20 +1,27 @@
-const salarioBruto = document.getElementById("Salario")
+const salarioInput = document.getElementById("salario")
+const botaoContainer = document.getElementById("button")
 let inss = 0
 
-if(salarioBruto <= 1621){
-    inss = salarioBruto * 0.075
-}else if(salarioBruto >= 1621 && salarioBruto <= 2902.84){
-    inss = (salarioBruto - 1621)*0.09 + 121.58
-}else if(salarioBruto > 2902.84 && salarioBruto <= 4354.27){
-    inss = (salarioBruto - 2902.84)*0.12 + 115.37 + 121.58
-}else if(salarioBruto > 4354.27 && salarioBruto <= 8475.55){
-    inss = (salarioBruto - 4354.27)*0.14 + 115.37 + 121.58 + 174.17
-}else{
-    inss = 988.10
+const botao = document.createElement("button")
+botao.textContent = "Calcular"
+botaoContainer.appendChild(botao)
+
+function calcular() {
+    let salarioBruto = parseFloat(salarioInput.value)
+    
+    if(salarioBruto <= 1621){
+        inss = salarioBruto * 0.075
+    }else if(salarioBruto <= 2902.84){
+        inss = (salarioBruto - 1621) * 0.09 + 121.58
+    }else if(salarioBruto <= 4354.27){
+        inss = (salarioBruto - 2902.84) * 0.12 + 115.37 + 121.58
+    }else if(salarioBruto <= 8475.55){
+        inss = (salarioBruto - 4354.27) * 0.14 + 115.37 + 121.58 + 174.17
+    }else{
+        inss = 988.10
+    }
+    
+    console.log(inss.toFixed(2))
 }
-<button id="button">Calcule</button>
 
-button.getElementById("button")
-addEventListener("click")
-
-console.log(inss.toFixed(2))
+botao.addEventListener("click", calcular)
